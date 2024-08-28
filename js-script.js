206383870f835ea1990a92cf2a9b1e5e55e8d9e4
holmeskone <[165698602+holmeskone@users.noreply.github.com]>
2024-08-28 14:34:27 +0200

    Add div inside container, selected by querySelector

---
+++ js-script.js
@@ -0,0 +1,7 @@
+// Create the div container that will contain all other squares
+const container = document.querySelector(".container");
+
+// Create one div (square) that will form the grid
+const square = document.createElement("div");
+square.classList.add("square");
+container.appendChild(square);
