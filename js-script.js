2e2cab89bb0283b0fea39a7193743ebf6f3ada1b
holmeskone <[165698602+holmeskone@users.noreply.github.com]>
2024-08-29 11:26:30 +0200

    Moved code so that sides can be overrriden when calling the function and is not dependant on the code

--- js-script.js
+++ js-script.js
@@ -2,19 +2,16 @@
 const container = document.querySelector(".container");
 
 // Create variable for number of sides
-let grid = 20;
-// Create variable for size of grid, this is used for the creation of squares later in the looping section.
-let gridSize = grid * grid;
+let sides;
 
-//Create CSS size of grid
-let gridPx = grid * 50;
-// Set the CSS variable --grid-px with the value of gridPx
-document.documentElement.style.setProperty('--grid-px', `${gridPx}px`);
-
-//Create divs function to create 16x16 
-const createSquares = (gridSize) => {
-    // Loop through size of grid
-    for (let i = 0; i < gridSize; i++) {
+//Create function to grid
+const createSquares = (sides) => {
+    //Create CSS size of grid - so that the grid is a square. 
+    let gridPx = sides * 50;
+    // Set the CSS variable --grid-px with the value of gridPx
+    document.documentElement.style.setProperty('--grid-px', `${gridPx}px`);
+    // Loop through size of grid. Size of grid side*side.
+    for (let i = 0; i < sides**2; i++) {
         // Create one div (square) that will form the grid
         const square = document.createElement("div");
         // Add class to div
@@ -24,4 +21,4 @@ const createSquares = (gridSize) => {
       }
 }
 
-createSquares(gridSize);
+createSquares(20);
