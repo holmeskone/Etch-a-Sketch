6a2d5a95c96a5ad0f377bff8bd2f0ae511814050
holmeskone <[165698602+holmeskone@users.noreply.github.com]>
2024-08-29 10:54:56 +0200

    Remove first div creation and add to function, it was creating problems with the grid because instead of 16x16=256 it was 257

--- js-script.js
+++ js-script.js
@@ -1,19 +1,20 @@
 // Create the div container that will contain all other squares
 const container = document.querySelector(".container");
 
-// Create one div (square) that will form the grid
-const square = document.createElement("div");
-square.classList.add("square");
-container.appendChild(square);
-
+// Create variable for number of sides
 let grid = 16;
+// Create variable for size of grid
 let gridSize = grid * grid;
 
 //Create divs function to create 16x16 
 const createSquares = (gridSize) => {
+    // Loop through size of grid
     for (let i = 0; i < gridSize; i++) {
+        // Create one div (square) that will form the grid
         const square = document.createElement("div");
+        // Add class to div
         square.classList.add("square");
+        // Make it child of container
         container.appendChild(square);;
       }
 }
